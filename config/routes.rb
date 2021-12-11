32644eca1fba89663894b61b08f871bf9c31ea9d
MACBOOK_MAKO <>
2021-12-11 23:06:53 +0900

    CREATE実装：genarate controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   get '/' => "top#top"
+
+  # get 'posts/show'
+  # get 'posts/create'
+  # get 'posts/edit'
+  # get 'posts/destroy'
+
+  resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
