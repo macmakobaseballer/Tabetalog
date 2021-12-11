cb83877a6203567b9615784357b125ea56b780cd
MACBOOK_MAKO <>
2021-12-11 15:17:54 +0900

    BagFix：タイムゾーン設定のfix

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,9 @@ module Tabetalog
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
+
   end
 end
