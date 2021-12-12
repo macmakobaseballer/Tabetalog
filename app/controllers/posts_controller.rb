ec45f746c0241d0f32c504fd07d74d6ee85650e5
MACBOOK_MAKO <>
2021-12-12 14:16:30 +0900

    CREATE処理実装：投稿画面描写

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,14 @@
 class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+  end
+
+  def edit
+  
+  end
 end
