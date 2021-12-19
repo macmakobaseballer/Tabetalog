1e36a0033649b8ff7f1de0b6d4305280fe0c361f
MACBOOK_MAKO <>
2021-12-19 17:23:53 +0900

    ログイン機能実装：登録フォーム処理実装

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   # get 'posts/destroy'
   get 'new' => "posts#new"
   resources :posts , only: [:index, :show, :create, :edit ,:update, :destroy]
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
